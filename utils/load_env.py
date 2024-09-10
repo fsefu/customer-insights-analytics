ed825979bbfa5e6e5e94eae9bcd1a2b1de2155bb
fsefu <fsefuwan@gmail.com>
2024-09-10 19:47:29 +0300

    "Updated load_environment function to load env variables from Streamlit secrets in production and from .env file in local development"

--- utils/load_env.py
+++ utils/load_env.py
@@ -1,14 +1,25 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 def load_environment():
     """
-    Load environment variables from a .env file into the system environment.
+    Load environment variables either from a .env file (for local development)
+    or from Streamlit secrets (for production).
     """
-    env_file = os.path.join(os.getcwd(), '.env')
-    
-    if os.path.exists(env_file):
-        load_dotenv(env_file)
-        print("Environment variables loaded successfully from .env file.")
+    if os.getenv('STREAMLIT_ENV') == 'production':
+        # Assuming production environment is identified by 'STREAMLIT_ENV' being set to 'production'
+        st.secrets["DB_NAME"] = os.getenv("DB_NAME", st.secrets["DB_NAME"])
+        st.secrets["DB_HOST"] = os.getenv("DB_HOST", st.secrets["DB_HOST"])
+        st.secrets["DB_USER"] = os.getenv("DB_USER", st.secrets["DB_USER"])
+        st.secrets["DB_PASSWORD"] = os.getenv("DB_PASSWORD", st.secrets["DB_PASSWORD"])
+        st.secrets["DB_PORT"] = os.getenv("DB_PORT", st.secrets["DB_PORT"])
+        print("Environment variables loaded from Streamlit secrets (production).")
     else:
-        raise FileNotFoundError(f"The .env file could not be found at {env_file}.")
+        # Load from .env file for local development
+        env_file = os.path.join(os.getcwd(), '.env')
+        if os.path.exists(env_file):
+            load_dotenv(env_file)
+            print("Environment variables loaded successfully from .env file (local development).")
+        else:
+            raise FileNotFoundError(f"The .env file could not be found at {env_file}.")
